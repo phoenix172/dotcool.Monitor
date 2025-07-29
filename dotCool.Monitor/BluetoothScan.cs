dd3c77c0cacf0f3d673ef55d00c264f54b5e9385
Phoenix172 <vani172@gmail.com>
2025-07-29 17:21:36 +0300

    Move to separate file

---
+++ dotCool.Monitor/BluetoothScan.cs
@@ -0,0 +1,23 @@
+﻿namespace dotCool.Monitor;
+
+public class BluetoothScan : IAsyncDisposable
+{
+    private readonly Func<Task> _cancellationAction;
+    private readonly Task _scanTask;
+
+    public BluetoothScan(Task scanTask, Func<Task> cancellationAction)
+    {
+        _cancellationAction = cancellationAction;
+        _scanTask = scanTask;
+    }
+
+    public async Task Await()
+    {
+        await _scanTask;
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        await _cancellationAction();
+    }
+}
--- dotCool.Monitor/LinuxBleClient.cs
+++ dotCool.Monitor/LinuxBleClient.cs
@@ -5,28 +5,6 @@ using Tmds.DBus;
 
 namespace dotCool.Monitor;
 
-public class BluetoothScan : IAsyncDisposable
-{
-    private readonly Func<Task> _cancellationAction;
-    private readonly Task _scanTask;
-
-    public BluetoothScan(Task scanTask, Func<Task> cancellationAction)
-    {
-        _cancellationAction = cancellationAction;
-        _scanTask = scanTask;
-    }
-
-    public async Task Await()
-    {
-        await _scanTask;
-    }
-
-    public async ValueTask DisposeAsync()
-    {
-        await _cancellationAction();
-    }
-}
-
 public class LinuxBleClient : IBleClient, IDisposable
 {
     private readonly ILogger<LinuxBleClient> _logger;
